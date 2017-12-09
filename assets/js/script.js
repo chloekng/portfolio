2e86495dd155a4212548ada925f43b154dde5345
Chloe <cngchloen@gmail.com>
2017-12-08 23:26:23 -0800

    fix prod scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,6 +124,21 @@ $(document).ready(function(){
 			$('.sidebar-r').css('display', 'none');			
 		}
 
+		for (let i = 0; i < (projSide.length); i++) {
+		//set i based on position in page? 
+		//make zones based on i positions??
+		if (window.pageYOffset > headingPos[i]) {
+		 		$(projSide[i]).css('background-position', 'left');
+				console.log(i)
+		 	} else if (window.pageYOffset < headingPos[i]) {
+		 		$(projSide[i]).css('background-position', 'right');
+		 		console.log(i);
+		 	} 
+		 }
+	}
+
+
+
 		// for (let i = 0; i < (projSide.length); i++) {
 			// if (window.pageYOffset > headingPos[i]) {
 			// 	$(projSide[i]).css('background-position', 'left');
@@ -132,58 +147,53 @@ $(document).ready(function(){
 		 // 		$(projSide[i]).css('background-position', 'right');
 		 // 		console.log(i);
 		 // 	} 
-		 console.log(headingPos);
-		 console.log(window.pageYOffset);
-			if (window.pageYOffset < headingPos[0]) {
-				$(projSide[0]).css('background-position', 'right');
-				console.log("zero");
-			} else if (window.pageYOffset > headingPos[0] && window.pageYOffset < headingPos[1]) {
-				$(projSide[0]).css('background-position', 'left');
-				$(projSide[1]).css('background-position', 'right');
-				console.log("one");
-			} else if (window.pageYOffset > headingPos[1] && window.pageYOffset < headingPos[2]) {
-				$(projSide[1]).css('background-position', 'left');
-				$(projSide[2]).css('background-position', 'right');
-				console.log("two");
-			} else if (window.pageYOffset > headingPos[2] && window.pageYOffset < headingPos[3]) {
-				$(projSide[2]).css('background-position', 'left');
-				$(projSide[3]).css('background-position', 'right');	
-				console.log("three");			
-			} else if (window.pageYOffset > headingPos[3] && window.pageYOffset < headingPos[4]) {
-				$(projSide[3]).css('background-position', 'left');
-				$(projSide[4]).css('background-position', 'right');	
-				console.log("four");
-			} else if (window.pageYOffset > headingPos[4] && window.pageYOffset < headingPos[5]) {
-				$(projSide[4]).css('background-position', 'left');
-				$(projSide[5]).css('background-position', 'right');	
-				console.log("five");
-			} else if (window.pageYOffset > headingPos[5] && window.pageYOffset < headingPos[6]) {
-				$(projSide[5]).css('background-position', 'left');
-				$(projSide[6]).css('background-position', 'right');	
-				console.log("six");
-			} else if (window.pageYOffset > headingPos[6] && window.pageYOffset < headingPos[7]) {
-				$(projSide[6]).css('background-position', 'left');
-				$(projSide[7]).css('background-position', 'right');	
-				console.log("seven");
-			} else if (window.pageYOffset > headingPos[7] && window.pageYOffset < headingPos[8]) {
-				$(projSide[7]).css('background-position', 'left');
-				$(projSide[8]).css('background-position', 'right');	
-				console.log("eight");
-			} else if (window.pageYOffset > headingPos[8] && window.pageYOffset < headingPos[9]) {
-				$(projSide[8]).css('background-position', 'left');
-				$(projSide[9]).css('background-position', 'right');	
-				console.log("nine");
-			} else if (window.pageYOffset > headingPos[9] && window.pageYOffset < headingPos[10]) {
-				$(projSide[9]).css('background-position', 'left');
-				$(projSide[10]).css('background-position', 'right');
-				console.log("ten");	
-			}
-		
-
-
- 
-
-
+		 // console.log(headingPos);
+		 // console.log(window.pageYOffset);
+			// if (window.pageYOffset < headingPos[0]) {
+			// 	$(projSide[0]).css('background-position', 'right');
+			// 	console.log("zero");
+			// } else if (window.pageYOffset > headingPos[0] && window.pageYOffset < headingPos[1]) {
+			// 	$(projSide[0]).css('background-position', 'left');
+			// 	$(projSide[1]).css('background-position', 'right');
+			// 	console.log("one");
+			// } else if (window.pageYOffset > headingPos[1] && window.pageYOffset < headingPos[2]) {
+			// 	$(projSide[1]).css('background-position', 'left');
+			// 	$(projSide[2]).css('background-position', 'right');
+			// 	console.log("two");
+			// } else if (window.pageYOffset > headingPos[2] && window.pageYOffset < headingPos[3]) {
+			// 	$(projSide[2]).css('background-position', 'left');
+			// 	$(projSide[3]).css('background-position', 'right');	
+			// 	console.log("three");			
+			// } else if (window.pageYOffset > headingPos[3] && window.pageYOffset < headingPos[4]) {
+			// 	$(projSide[3]).css('background-position', 'left');
+			// 	$(projSide[4]).css('background-position', 'right');	
+			// 	console.log("four");
+			// } else if (window.pageYOffset > headingPos[4] && window.pageYOffset < headingPos[5]) {
+			// 	$(projSide[4]).css('background-position', 'left');
+			// 	$(projSide[5]).css('background-position', 'right');	
+			// 	console.log("five");
+			// } else if (window.pageYOffset > headingPos[5] && window.pageYOffset < headingPos[6]) {
+			// 	$(projSide[5]).css('background-position', 'left');
+			// 	$(projSide[6]).css('background-position', 'right');	
+			// 	console.log("six");
+			// } else if (window.pageYOffset > headingPos[6] && window.pageYOffset < headingPos[7]) {
+			// 	$(projSide[6]).css('background-position', 'left');
+			// 	$(projSide[7]).css('background-position', 'right');	
+			// 	console.log("seven");
+			// } else if (window.pageYOffset > headingPos[7] && window.pageYOffset < headingPos[8]) {
+			// 	$(projSide[7]).css('background-position', 'left');
+			// 	$(projSide[8]).css('background-position', 'right');	
+			// 	console.log("eight");
+			// } else if (window.pageYOffset > headingPos[8] && window.pageYOffset < headingPos[9]) {
+			// 	$(projSide[8]).css('background-position', 'left');
+			// 	$(projSide[9]).css('background-position', 'right');	
+			// 	console.log("nine");
+			// } else if (window.pageYOffset > headingPos[9] && window.pageYOffset < headingPos[10]) {
+			// 	$(projSide[9]).css('background-position', 'left');
+			// 	$(projSide[10]).css('background-position', 'right');
+			// 	console.log("ten");	
+			// }
+	
 
 			// if (value > headingPos[i]) {
 			// 	$(projSide[i]).css('background-position', 'left');
